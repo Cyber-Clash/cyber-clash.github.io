0ad8778fe019217e99c4c98ff34ca0278e96112c
CyberClash <126948978+Cyber-Clash@users.noreply.github.com>
2023-03-12 13:18:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+    xml:lang="<?php echo $this->language; ?>"
+    lang="<?php echo $this->language; ?>" >
+        <head>
+            <title>cyber-clash.github.io</title>
+        </head>
+    <body>
+        TEST
+    </body>
 <html>
-  <head>
-    <title>cyber-clash.github.io</title>
-  </head>
-  <body>
-    TEST
-  </body>
-</html>
